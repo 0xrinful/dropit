ca168683eb142fc42a3958a37afa461bf5f8d140
0xrinful <0xrinful@gmail.com>
2025-08-25 07:26:37 +0300

    sql migrations

---
+++ migrations/000002_create_files_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS files (
+  id bigserial PRIMARY KEY,
+  token char(8) UNIQUE NOT NULL,
+  owner_id bigint REFERENCES users (id) ON DELETE SET NULL,
+  filename TEXT NOT NULL,
+  storage_path TEXT NOT NULL UNIQUE,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  last_accessed_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  download_count bigint NOT NULL DEFAULT 0
+);
